8028d3c8c8151cfc211f9c10ba068d6d9acc98d8
David Muro <david.muro9@gmail.com>
2018-12-03 18:21:14 +0100

    project already set up

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody2D theRB;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        theRB.velocity = new Vector2(1f, 1f);
+	}
+}
