63f37dbffb5ec9eaa43676a96824c2ecc9756c95
David Muro <david.muro9@gmail.com>
2018-12-03 21:05:36 +0100

    collisions and changing scenes

---
+++ Assets/Scripts/AreaEntrance.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaEntrance : MonoBehaviour {
+	public string transitionName;
+
+	// Use this for initialization
+	void Start () {
+		if (!string.IsNullOrEmpty(transitionName) && 
+			transitionName == PlayerController.instance.areaTransitionName) {
+			PlayerController.instance.transform.position = transform.position;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public Rigidbody2D theRB;
-
+	public float moveSpeed;
+	public Animator myAnim;
+	public static PlayerController instance;
+	public string areaTransitionName;
+	
 	// Use this for initialization
 	void Start () {
-		
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        theRB.velocity = new Vector2(1f, 1f);
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+
+		myAnim.SetFloat("moveX", theRB.velocity.x);
+		myAnim.SetFloat("moveY", theRB.velocity.y);
+
+		if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 ||
+			Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1) {
+			myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+			myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+		}
 	}
 }
