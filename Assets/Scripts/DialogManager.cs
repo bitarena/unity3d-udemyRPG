1bef867442d43b3e8307eb921808433eb0494c2e
David Muro <david.muro9@gmail.com>
2018-12-08 18:42:45 +0100

    dialogs between two characters working

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -10,24 +10,36 @@ public class DialogManager : MonoBehaviour {
 	public GameObject nameBox;
 	public string[] dialogLines;
 	public int currentLine;
+	public static DialogManager instance;
+	private bool justStarted;
 
 	// Use this for initialization
 	void Start () {
-		dialogText.text = dialogLines[currentLine];
+		instance = this;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (dialogBox.activeInHierarchy) {
-			if (Input.GetButtonUp("Fire1")) {
+			if (Input.GetButtonUp("Fire1") && !justStarted) {
 				currentLine++;
 
 				if (currentLine >= dialogLines.Length) {
 					dialogBox.SetActive(false);
 				} else {
-				dialogText.text = dialogLines[currentLine];
+					dialogText.text = dialogLines[currentLine];
 				}
+			} else if (Input.GetButtonUp("Fire1")) {
+				justStarted = false;
 			}
 		}
+	}
+
+	public void ShowDialog(string[] newLines) {
+		dialogLines = newLines;
+		currentLine = 0;
+		dialogText.text = dialogLines[currentLine];
+		dialogBox.SetActive(true);
+		justStarted = true;
 	}
 }
