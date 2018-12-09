dec5a143c52de6910a5c03ce66e1b508ef762247
David Muro <david.muro9@gmail.com>
2018-12-09 19:34:28 +0100

    added player stats

---
+++ Assets/Scripts/CharStats.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour {
+	public string charName;
+	public int playerLevel = 1;
+	public int currentExp;
+	
+
+	public int currentHP;
+	public int maxHP = 100;
+	public int currentMP;
+	public int maxMP = 30;
+	public int strenght;
+	public int defense;
+	public int weaponPower;
+	public int arorPower;
+	public string equippedWeapon;
+	public string equippedArmor;
+	public Sprite charImage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
